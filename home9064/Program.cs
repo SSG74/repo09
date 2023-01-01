5d94ada11934cfe9ca4c4f6a8d2c16f935dc793c
Svutlana <fire_fly_74@mail.ru>
2023-01-01 21:11:46 +0500

    home9064

---
+++ home9064/Program.cs
@@ -0,0 +1,27 @@
+﻿/// 64 Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1.
+// Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+int NaturalNumber(int num)
+{
+    if (num==1) return 1;
+    else 
+    {
+        Console.Write($"{NaturalNumber(num-1)}, ");
+        return num;
+    }
+}
+
+int EnterData(string text)
+{
+    Console.WriteLine(text);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+} 
+// Ввести число N
+int num = EnterData("Введите число N: ");
+// найти натуральные числа
+NaturalNumber(num);
+// вывести натуральные числа
+Console.WriteLine(num); 
